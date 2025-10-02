25f568b3fda5d88e3a689c778d909f6ab3282d87
Ruan-Marcilio <rmsl@discente.ifpe.edu.br>
2025-10-01 21:34:40 -0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,24 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.seutema.ui.theme.SeuTemaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SeuTemaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TasksScreen()
+                }
+            }
+        }
+    }
+}
